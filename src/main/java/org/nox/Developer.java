d4c68897a1d0c4071db1260ed67ad7f40d44d627
vchumachenko <v.chumachenko@yahoo.com>
2023-10-03 22:10:57 +0300

    master Done

--- src/main/java/org/nox/Developer.java
+++ src/main/java/org/nox/Developer.java
@@ -1,2 +1,29 @@
-package org.nox;public class Developer {
+package org.nox;
+
+import java.util.List;
+
+public class Developer {
+    private String name;
+    private List<String> languages;
+
+    public Developer(String name, List<String> languages) {
+        this.name = name;
+        this.languages = languages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<String> languages) {
+        this.languages = languages;
+    }
 }
--- src/main/java/org/nox/Main.java
+++ src/main/java/org/nox/Main.java
@@ -1,7 +1,38 @@
 package org.nox;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+
+    Developer dev1 = new Developer("Наташа", Arrays.asList("Java", "C++"));
+    Developer dev2 = new Developer("Эрнест", Arrays.asList("Java", "Python"));
+    Developer dev3 = new Developer("Элла", Arrays.asList("С#", "Python", "JavaScript"));
+
+    Stream<Developer> developerStream = Stream.of(dev1, dev2, dev3);
+
+    // Создаю Set для хранения языков программирования
+    Set<String> uniqLanguages = new HashSet<>();
+
+
+    developerStream.filter(developer -> {
+      // Временный set для хранения языков текущего разработчика
+      HashSet<String> tmp = new HashSet<>(developer.getLanguages());
+
+      // Сохраняю только те языки, которые уже не были добавлены в uniqLanguages
+      tmp.retainAll(uniqLanguages);
+
+      // true, если временное множество пусто (разработчик использует уникальные языки)
+      return tmp.isEmpty();
+    }).forEach(developer -> {
+      System.out.println(developer.getName());
+
+      // Добавляю языки текущего разработчика в множество uniqLanguages
+      uniqLanguages.addAll(developer.getLanguages());
+    });
+  }
 }
+
